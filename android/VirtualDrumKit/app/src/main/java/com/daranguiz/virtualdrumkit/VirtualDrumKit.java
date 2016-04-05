c4fe513b02f18d5002f73d9dedc80ce6fd532795
Dario Aranguiz <aranguizdario@gmail.com>
2016-04-04 20:15:12 -0500

    Added buttons

--- android/VirtualDrumKit/app/src/main/java/com/daranguiz/virtualdrumkit/VirtualDrumKit.java
+++ android/VirtualDrumKit/app/src/main/java/com/daranguiz/virtualdrumkit/VirtualDrumKit.java
@@ -2,12 +2,39 @@ package com.daranguiz.virtualdrumkit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class VirtualDrumKit extends AppCompatActivity {
+
+    private TextView textStatus;
+    private Button buttonStart;
+    private Button buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_virtual_drum_kit);
+
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+
+        textStatus.setText("Stopped!");
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textStatus.setText("Started!");
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textStatus.setText("Stopped!");
+            }
+        });
     }
 }
