1503dce08843417751b51bf410fdfa9cd7e64c91
Dario Aranguiz <aranguizdario@gmail.com>
2016-04-05 12:16:42 -0500

    Added CSV filter, resample

--- scripts/matlab/filter_gen.m
+++ scripts/matlab/filter_gen.m
@@ -1,35 +1,37 @@
-%% FIR Filter Generation
-% Dario Aranguiz
-close all;
+function b = filter_gen(Fs, Fc, transBand, n)
 
-n = 10;            % Number of taps 
-Fs = 25;           % Sampling Rate
-Fc = 12;           % Cutoff Frequency
-transBand = 4;     % Width of transition band
+    % n = 10;            % Number of taps 
+    % Fs = 25;           % Sampling Rate
+    % Fc = 12;           % Cutoff Frequency
+    % transBand = 4;     % Width of transition band
 
-% Construct filter specification
-f = [0, Fc / Fs, (Fc + transBand) / Fs, 1];
-a = [1, 1, 0, 0];
+    % Construct filter specification
+    f = [0, Fc / Fs, (Fc + transBand) / Fs, 1];
+    a = [1, 1, 0, 0];
 
-% Parks-McClellan Method
-b = firpm(n, f, a, [1 1]);
+    % Parks-McClellan Method
+    b = firpm(n, f, a, [1 1]);
 
-% Window-based Method
-% b = fir1(n, Fc);
+    % Window-based Method
+    % b = fir1(n, Fc);
 
-% Frequency Sampling-based Method
-% b = fir2(n, f, a);
+    % Frequency Sampling-based Method
+    % b = fir2(n, f, a);
 
-% Plot filter response over digital frequency range
-[h,w] = freqz(b, 1);
-plot(f, a, w/pi, abs(h));
+    % Plot filter response over digital frequency range
+    figure;
+    [h,w] = freqz(b, 1);
+    plot(f, a, w/pi, abs(h));
+    title('Frequency Response');
 
-% String construction
-javaString = '\nprivate final float[] coefs = {';
-for i=1:n
-    javaString = strcat(javaString, num2str(b(i)), ', ');
+    % String construction
+    javaString = '\nprivate final float[] coefs = {';
+    for i=1:n
+        javaString = strcat(javaString, num2str(b(i)), ', ');
+    end
+    javaString(end-1:end) = '};';
+    javaString = strcat(javaString, '\n');
+
+    fprintf(javaString);
+
 end
-javaString(end-1:end) = '};';
-javaString = strcat(javaString, '\n');
-
-fprintf(javaString);
